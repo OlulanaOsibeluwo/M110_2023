1813b26ed74f6d7cf24cfaa4e8b73e83371e7da0
zhl26 <123880973+zhl26@users.noreply.github.com>
2023-10-23 11:22:22 +0100

    Add files via upload
    zh massive brain

---
+++ zh_linefollowing/misc_functions.ino
@@ -0,0 +1,80 @@
+void left90(){//90 degrees left turn, only works if turning onto a line
+  //need to change this when we get new chassis lol
+  //turn left, pivot on L wheel
+  left_motor(0,1);
+  // Lmotor_fw = true;
+  Lmotor_speed = 0; //this vs run ->release?
+  right_motor(255,1);
+    Rmotor_fw = true;
+    Rmotor_speed = 255;
+  // 1 and 0 here are ALSO FLIPPED FOR PRINTED TEST
+  //note that this only works if right front sensor is not off the line before turning!!!!!!
+  while (digitalRead(4) ==1) // turn until right off line
+    continue;
+  while (digitalRead(4)==0) //and then until on the new line
+    continue;
+}
+void right90(){//90 degrees right turn
+  //turn right, pivot on r wheel
+  right_motor(0,1);
+  // Rmotor_fw = true;
+  Rmotor_speed = 0; //this vs run ->release?
+  left_motor(255,1);
+    Lmotor_fw = true;
+    Lmotor_speed = 255;
+  // 1 and 0 here are ALSO FLIPPED FOR PRINTED TEST
+  while (digitalRead(3) ==1) // turn until left off line
+    continue;
+  while (digitalRead(3)==0) 
+    continue;
+}
+void junctionHandler(){
+  currentJunction++;
+  //query tof sensor to check for block, if block found check with ultrasound, then reversejunctionhandler()
+  //if junction number > 11, jnumber = jnumber - 10 (for the loop?)
+  //for now counts up to 13 just to go back to start zone
+  if (currentJunction == 1||currentJunction == 3||currentJunction == 6||
+          currentJunction == 7||currentJunction == 8||currentJunction == 11||currentJunction == 13){
+    
+    ;//do nothing
+  }
+
+  else if (currentJunction == 2||currentJunction == 12){
+    //turn right
+    right90();
+  }
+
+  else if (currentJunction == 4||currentJunction == 5||currentJunction == 9||currentJunction == 10){ //settle this
+    //turn left
+    left90();
+  }
+
+  int v_leftVal, leftVal, rightVal, v_rightVal;
+  do{
+    //line follow straight to leave the junction n not re-read the same junction
+    
+    light_sequence(v_leftVal, leftVal, rightVal, v_rightVal);
+
+    if (leftVal == 1 && rightVal == 1) {
+        // Forward motion
+      go_straight();
+      
+      }
+    else if (leftVal == 0 && rightVal == 0) { //shouldnt happen but this could be real bad, turn back a bit as a contingency??
+        go_straight();
+      }
+
+    else if (leftVal == 0 && rightVal == 1){
+      //adjust to the right
+      slant_right();
+      }
+
+    else if (leftVal == 1 && rightVal == 0) {
+      //adjust to the left
+      slant_left();
+      }
+  }while (v_leftVal==1||v_rightVal==1);
+}
+
+//reverse junction handler(), uturn, decrement currentJunction, turn opposite way, etc back to start!
+//then block delivery functionality
---
+++ zh_linefollowing/zh_linefollowing.ino
@@ -0,0 +1,197 @@
+#include <Adafruit_MotorShield.h>
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *left = AFMS.getMotor(3);
+Adafruit_DCMotor *right = AFMS.getMotor(4);
+
+//linesensors pins GET RID OF THIS
+// int v_LeftLineSensorPin = 2; 
+// int LeftLineSensorPin = 3;
+// int RightLineSensorPin = 4; 
+// int v_RightLineSensorPin = 5;
+
+bool Lmotor_fw, Rmotor_fw = false;
+int Lmotor_speed, Rmotor_speed = 0; //is there a better way to do this?? without the global vars
+int currentJunction = 0;
+
+
+
+void light_sequence(int &v_leftVal, int &leftVal, int &rightVal, int &v_rightVal) {
+  v_leftVal = digitalRead(2); 
+  leftVal = digitalRead(3);
+  rightVal = digitalRead(4);
+  v_rightVal = digitalRead(5);
+
+// //ONLY FOR NEGATIVE PRINT TESTING, RMB TO DELETE
+//   v_leftVal = 1 - v_leftVal; 
+//   leftVal = 1-leftVal;
+//   rightVal = 1 - rightVal;
+//   v_rightVal = 1-v_rightVal;
+// //ONLY FOR NEGATIVE PRINT TESTING, RMB TO DELETE
+
+
+  Serial.print(v_leftVal);
+  Serial.print(leftVal);
+  Serial.print(rightVal);
+  Serial.println(v_rightVal);
+
+
+}
+
+void left_motor(int rpm, int direction){
+  left->setSpeed(rpm);
+  if (direction == 1) {
+    left->run(FORWARD);}
+  else {
+    left->run(BACKWARD);
+  }
+}
+
+void right_motor(int rpm, int direction){
+  right->setSpeed(rpm);
+  if (direction == 1) {
+  right->run(FORWARD);}
+  else {
+    right->run(BACKWARD);
+  }
+}
+
+
+void turn_left(int rpm){
+  // Adjust to the right
+  left->setSpeed(rpm);
+  right->setSpeed(rpm);
+  left->run(FORWARD);
+  right->run(BACKWARD);
+}
+
+void turn_right(int rpm){
+  // Adjust to the right
+  left->setSpeed(rpm);
+  right->setSpeed(rpm);
+  left->run(BACKWARD);
+  right->run(FORWARD);
+}
+
+void forward(int rpm){
+  // left_motor(rpm, 1);
+  // right_motor(rpm,1);
+  left->setSpeed(rpm);
+  right->setSpeed(rpm);
+  left->run(FORWARD);
+  right->run(FORWARD);
+
+}
+void go_straight(){
+  if (Lmotor_fw!=true||Lmotor_speed != 255){
+    left_motor(255,1);
+    Lmotor_fw = true;
+    Lmotor_speed = 255;}
+ 
+
+  if (Rmotor_fw!=true||Rmotor_speed != 255){
+    right_motor(255,1);
+    Rmotor_fw = true;
+    Rmotor_speed = 255;}  
+}
+void slant_right(){
+  if (Lmotor_fw!=true || Lmotor_speed != 255){
+    left_motor(255,1);
+    Lmotor_fw = true;
+    Lmotor_speed = 255;}
+ 
+  if (Rmotor_fw!=true||Rmotor_speed != 50){
+    right_motor(50,1);
+    Rmotor_fw = true;
+    Rmotor_speed = 50;}
+
+}
+void slant_left(){
+  if (Rmotor_fw!=true||Rmotor_speed != 255){
+    right_motor(255,1);
+    Rmotor_fw = true;
+    Rmotor_speed = 255;}
+             
+  if (Lmotor_fw!=true||Lmotor_speed != 50){
+    left_motor(50,1);
+    Lmotor_speed = 50;
+  }
+}
+
+
+void reverse(int rpm){
+  left_motor(rpm, 0);
+  right_motor(rpm, 0);
+}
+
+void stop(){
+  // cuts power to motor, does not brake
+  left->run(RELEASE);
+  right->run(RELEASE);                                    
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  //Pins for Line Sensors 
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(4,INPUT);
+  pinMode(5,INPUT);
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.")                                                   ;
+    while (1); 
+  }
+
+  Serial.println("Motor Shield found.");
+
+  //initiating speed and direction of wheels
+  // left_motor(200, 1);
+  // right_motor(200, 1);
+
+
+
+  delay(2000);
+  Serial.println("kk here we go");
+}
+
+void loop(){
+// Variables to store sensor values
+int v_leftVal, leftVal, rightVal, v_rightVal;
+ 
+                                  // Read sensor values
+light_sequence(v_leftVal, leftVal, rightVal, v_rightVal);
+
+if (v_leftVal == 0 && leftVal == 1 && rightVal == 1 && v_rightVal == 0) {
+    // Forward motion
+  go_straight();
+  
+  }
+else if (v_leftVal == 0 && leftVal == 0 && rightVal == 0 && v_rightVal == 0) { //check whether going off lines is a problem
+    go_straight();
+  }
+
+else if (v_leftVal == 0 && leftVal == 0 && rightVal == 1 && v_rightVal == 0){
+  //adjust to the right
+  slant_right();
+  }
+
+else if (v_leftVal == 0 && leftVal == 1 && rightVal == 0 && v_rightVal == 0) {
+  //adjust to the left
+  slant_left();
+  }
+
+
+else if (v_leftVal == 1||v_rightVal == 1){
+  //junction met pog
+
+  junctionHandler();
+
+}
+
+
+}
